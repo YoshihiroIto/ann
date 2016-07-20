9007042fa59b849503368d811e939d299df4c6fd
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-07-21 07:36:40 +0900

    TimeMeasure 追加

---
+++ Ann.Foundation/TimeMeasure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Ann.Foundation
+{
+    public class TimeMeasure : IDisposable
+    {
+        private readonly Stopwatch _stopwatch;
+
+        public TimeMeasure()
+        {
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+        }
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+            Debug.WriteLine($"TimeMeasure : {_stopwatch.ElapsedMilliseconds}ms : {_stopwatch.ElapsedTicks}");
+        }
+    }
+}
