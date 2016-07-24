0b6173ada73835dcd57c05d12e59c4bb50cd3270
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-07-24 13:00:47 +0900

    オープンソースリストを自動生成するようにした

--- Ann.Foundation/Constants.cs
+++ Ann.Foundation/Constants.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Ann.Foundation.Properties;
+using YamlDotNet.Serialization;
 
 namespace Ann.Foundation
 {
@@ -20,89 +23,29 @@ namespace Ann.Foundation
                 ? Environment.GetEnvironmentVariable("PROGRAMFILES(X86)")
                 : Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
 
-        public static readonly OpenSource[] OpenSources =
+        private static OpenSource[] _OpenSources;
+
+        public static OpenSource[] OpenSources
         {
-            new OpenSource
+            get
             {
-                Name = "FlatBuffers",
-                Auther = "Google",
-                Summry = "Memory Efficient Serialization Library",
-                Url = "https://github.com/google/flatbuffers"
-            },
-            new OpenSource
-            {
-                Name = "LRU Cache",
-                Auther = "Yoshihiro Ito",
-                Summry = "Simple Implementation C# LRU Cache",
-                Url = "https://github.com/YoshihiroIto/Jewelry"
-            },
-            new OpenSource
-            {
-                Name = "Hardcodet WPF NotifyIcon",
-                Auther = "Philipp Sumi",
-                Summry =
-                    "This is an implementation of a NotifyIcon (aka system tray icon or taskbar icon) for the WPF platform. It does not just rely on the Windows Forms NotifyIcon component, but is a purely independent control which leverages several features of the WPF framework in order to display rich tooltips, popups, context menus, and balloon messages. It can be used directly in code or embedded in any XAML file.",
-                Url = "http://www.hardcodet.net/wpf-notifyicon"
-            },
-            new OpenSource
-            {
-                Name = "Livet Cask",
-                Auther = "Livet Project",
-                Summry = "Livet(リベット)はWPF4のためのMVVM(Model/View/ViewModel)パターン用インフラストラクチャです。",
-                Url = "http://ugaya40.hateblo.jp/entry/Livet"
-            },
-            new OpenSource
-            {
-                Name = "Material Design In XAML Toolkit",
-                Auther = "James Willock",
-                Summry = "Material Design styles for all major WPF Framework controls",
-                Url = "https://github.com/ButchersBoy/MaterialDesignInXamlToolkit"
-            },
-            new OpenSource
-            {
-                Name = "Material Design Icons",
-                Auther = "Austin Andrews",
-                Summry =
-                    "Material Design Icons' growing icon collection allows designers and developers targeting various platforms to download icons in the format, color and size they need for any project.",
-                Url = "https://materialdesignicons.com/"
-            },
-            new OpenSource
-            {
-                Name = "ReactiveProperty",
-                Auther = "neuecc xin9le okazuki",
-                Summry = "ReactiveProperty is MVVM and Asynchronous Extensions for Reactive Extensions(Rx-Main).",
-                Url = "https://github.com/runceel/ReactiveProperty"
-            },
-            new OpenSource
-            {
-                Name = "Reactive Extensions",
-                Auther = ".NET Foundation and Contributors",
-                Summry =
-                    "Reactive Extensions Main Library combining the interfaces, core, LINQ, and platform services libraries.",
-                Url = "https://github.com/Reactive-Extensions/Rx.NET"
-            },
-            new OpenSource
-            {
-                Name = "Interactive Extensions",
-                Auther = ".NET Foundation and Contributors",
-                Summry = "Interactive Extensions Main Library used to express queries over enumerable sequences.",
-                Url = "https://github.com/Reactive-Extensions/Rx.NET"
-            },
-            new OpenSource
-            {
-                Name = "Windows API Code Pack",
-                Auther = "Aybe",
-                Summry = "Windows API Code Pack 1.1",
-                Url = "https://github.com/aybe/Windows-API-Code-Pack-1.1"
-            },
-            new OpenSource
-            {
-                Name = "YamlDotNet",
-                Auther = "Antoine Aubry",
-                Summry = "A .NET library for YAML.",
-                Url = "http://aaubry.net/pages/yamldotnet.html"
-            },
-        };
+                if (_OpenSources != null)
+                    return _OpenSources;
+
+                try
+                {
+                    var yaml = System.Text.Encoding.UTF8.GetString(Resources.OpenSourceList);
+                    using (var reader = new StringReader(yaml))
+                        _OpenSources = new Deserializer().Deserialize<OpenSource[]>(reader);
+
+                    return _OpenSources;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
     }
 
     public enum IndexOpeningResults
---
+++ Ann.GenOpenSourceList/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Ann.Foundation;
+using Newtonsoft.Json;
+using YamlDotNet.Serialization;
+
+namespace Ann.GenOpenSourceList
+{
+    class Program
+    {
+        static void Main()
+        {
+            var yaml = Generate(@"..\..\..\Ann\packages.config");
+            File.WriteAllText(@"..\..\..\Ann.Foundation\OpenSourceList.yaml", yaml);
+        }
+
+        private static string Generate(string packagesConfigPath)
+        {
+            var packages = XElement.Load(packagesConfigPath).Elements("package");
+            var openSources = packages.Select(p => GeneratePackage(p.Attribute("id").Value)).ToList();
+
+            // nuget   以：外
+            openSources.Add(
+                new OpenSource
+                {
+                    Name = "FlatBuffers",
+                    Auther = "Google",
+                    Summry = "Memory Efficient Serialization Library",
+                    Url = "https://github.com/google/flatbuffers"
+                });
+
+            openSources.Add(
+                new OpenSource
+                {
+                    Name = "LRU Cache",
+                    Auther = "Yoshihiro Ito",
+                    Summry = "Simple Implementation C# LRU Cache",
+                    Url = "https://github.com/YoshihiroIto/Jewelry"
+                });
+
+            using (var writer = new StringWriter())
+            {
+                new Serializer(SerializationOptions.EmitDefaults).Serialize(writer, openSources.OrderBy(x => x.Name));
+                return writer.ToString();
+            }
+        }
+
+        private static OpenSource GeneratePackage(string id)
+        {
+            var url = $"http://api-v2v3search-0.nuget.org/query?q={id}";
+
+            using (var wc = new System.Net.WebClient())
+            {
+                wc.Encoding = System.Text.Encoding.UTF8;
+                var jsonText = wc.DownloadString(url);
+                var json = JsonConvert.DeserializeObject<Rootobject>(jsonText);
+
+                var data = json.data.Single(x => x.id == id);
+
+                return new OpenSource
+                {
+                    Name = data.title,
+                    Auther = string.Join(" ", data.authors),
+                    Summry = string.IsNullOrEmpty(data.summary) ? data.description : data.summary,
+                    Url = data.projectUrl
+                };
+            }
+        }
+
+        // ReSharper disable InconsistentNaming
+        public class Rootobject
+        {
+            public Context context { get; set; }
+            public int totalHits { get; set; }
+            public DateTime lastReopen { get; set; }
+            public string index { get; set; }
+            public Datum[] data { get; set; }
+        }
+
+        public class Context
+        {
+            public string vocab { get; set; }
+            public string _base { get; set; }
+        }
+
+        public class Datum
+        {
+            public string id { get; set; }
+            public string type { get; set; }
+            public string registration { get; set; }
+            //public string id { get; set; }
+            public string version { get; set; }
+            public string description { get; set; }
+            public string summary { get; set; }
+            public string title { get; set; }
+            public string iconUrl { get; set; }
+            public string licenseUrl { get; set; }
+            public string projectUrl { get; set; }
+            public string[] tags { get; set; }
+            public string[] authors { get; set; }
+            public int totalDownloads { get; set; }
+            public Version[] versions { get; set; }
+        }
+
+        public class Version
+        {
+            public string version { get; set; }
+            public int downloads { get; set; }
+            public string id { get; set; }
+        }
+
+        // ReSharper restore InconsistentNaming
+    }
+}
