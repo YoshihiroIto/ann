782c42a8e4a7c98ff04d6587dec727bcaaf2defc
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-03 19:38:26 +0900

    ProcessHelperTest, TimeMeasureTest追加

---
+++ Ann.Foundation.Test/ProcessHelperTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ann.Foundation.Test
+{
+    [TestClass]
+    public class ProcessHelperTest
+    {
+        private static string app = @"..\..\..\Ann.GenOpenSourceList\Ann.GenOpenSourceList.exe";
+
+        [TestMethod]
+        public void Simple()
+        {
+            ProcessHelper.RunAsync(app, "", false).Wait();
+        }
+
+        [TestMethod]
+        public void IgnoreError()
+        {
+            ProcessHelper.RunAsync("XXXXX", "", false).Wait();
+        }
+
+        [TestMethod]
+        public void RunAsAdmin()
+        {
+            ProcessHelper.RunAsync(app, "", true).Wait();
+        }
+    }
+}
---
+++ Ann.Foundation.Test/TimeMeasureTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ann.Foundation.Test
+{
+    [TestClass]
+    public class TimeMeasureTest
+    {
+        [TestMethod]
+        public void Simple()
+        {
+            using (new TimeMeasure())
+            {
+            }
+        }
+
+        [TestMethod]
+        public void Title()
+        {
+            using (new TimeMeasure("TimeMeasureTest"))
+            {
+            }
+        }
+    }
+}
