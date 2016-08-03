643b9bc078d2f653519e3dc3392f5f2b92271b21
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-03 19:57:23 +0900

    CompositDisposableExtensionTest 追加

---
+++ Ann.Foundation.Test/CompositDisposableExtensionTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Reactive.Disposables;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ann.Foundation.Test
+{
+    [TestClass]
+    public class CompositDisposableExtensionTest
+    {
+        [TestMethod]
+        public void Action()
+        {
+            var i = 100;
+
+            Assert.AreEqual(i, 100);
+
+            using (var cd = new CompositeDisposable())
+            {
+                Assert.AreEqual(i, 100);
+
+                cd.Add(() => i ++);
+
+                Assert.AreEqual(i, 100);
+            }
+
+            Assert.AreEqual(i, 101);
+        }
+    }
+}
