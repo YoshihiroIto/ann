7d7f122546e89980e7431363d9e3d22d6d2ee355
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-05 00:57:56 +0900

    HotKeyRegisterTest, OpenSourceTest追加

---
+++ Ann.Foundation.Test/HotKeyRegisterTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Xunit;
+
+namespace Ann.Foundation.Test
+{
+    public class HotKeyRegisterTest
+    {
+        [StaFact]
+        public void Simple()
+        {
+            var w = new Window();
+
+            using (var r = new HotKeyRegister(ModifierKeys.Alt, Key.Up, w))
+            {
+                Assert.True(r.Register());
+
+                r.Unregister();
+            }
+
+            using (var r = new HotKeyRegister(ModifierKeys.Alt, Key.Up, w))
+            {
+                Assert.True(r.Register());
+            }
+        }
+
+        [StaFact]
+        public void AlreadyRegister()
+        {
+            var w = new Window();
+
+            using (var r = new HotKeyRegister(ModifierKeys.Alt, Key.Up, w))
+            {
+                Assert.True(r.Register());
+                Assert.True(r.Register());
+            }
+        }
+
+        [StaFact]
+        public void AlreadyUsed()
+        {
+            var w = new Window();
+
+            using (var r1 = new HotKeyRegister(ModifierKeys.Alt, Key.Up, w))
+            {
+                Assert.True(r1.Register());
+
+                using (var r2 = new HotKeyRegister(ModifierKeys.Alt, Key.Up, w))
+                {
+                    Assert.False(r2.Register());
+                }
+            }
+        }
+    }
+}
---
+++ Ann.Foundation.Test/OpenSourceTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace Ann.Foundation.Test
+{
+    public class OpenSourceTest
+    {
+        [Fact]
+        public void Simple()
+        {
+            var o = new OpenSource
+            {
+                Auther = "A",
+                Name = "B",
+                Summry = "C",
+                Url = "D"
+            };
+
+            Assert.Equal(o.Auther, "A");
+            Assert.Equal(o.Name, "B");
+            Assert.Equal(o.Summry, "C");
+            Assert.Equal(o.Url, "D");
+        }
+    }
+}
