e24c7bc40338139fb6062d11304d74a42a4e8d18
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-05 16:29:11 +0900

    TypeofConverterTest 追加

---
+++ Ann.Foundation.Test/Control/ValueConverter/TypeofConverterTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Ann.Foundation.Control.ValueConverter;
+using Xunit;
+
+namespace Ann.Foundation.Test.Control.ValueConverter
+{
+    public class TypeofConverterTest
+    {
+        [Fact]
+        public void Convert()
+        {
+            var c = new TypeofConverter();
+
+            var t = c.Convert("abc", null, null, null);
+
+            Assert.Equal(t, typeof(string));
+        }
+
+        [Fact]
+        public void ConvertBack()
+        {
+            var c = new TypeofConverter();
+
+            Assert.Throws<NotImplementedException>(() =>
+                c.ConvertBack("abc", null, null, null));
+        }
+    }
+}
