9375b86268c03501e0c805d013134c91da2c8b83
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-06 11:32:36 +0900

    Config/MainWindowTest, Config/MostRecentUsedListTest追加

---
+++ Ann.Core.Test/Config/MostRecentUsedListTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using Ann.Core.Config;
+using Xunit;
+
+namespace Ann.Core.Test.Config
+{
+    public class MostRecentUsedListTest
+    {
+        [Fact]
+        public void Basic()
+        {
+            var c = new MostRecentUsedList();
+
+            Assert.NotNull(c.AppPath);
+            Assert.Equal(0, c.AppPath.Count);
+
+            c.AppPath = new ObservableCollection<string>
+            {
+                "AAA"
+            };
+            Assert.Equal(1, c.AppPath.Count);
+            Assert.Equal("AAA", c.AppPath[0]);
+        }
+    }
+}
