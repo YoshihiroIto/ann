45d613d3e7fa36a47fcaa7a76d4a15ee4e34ea8d
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-06 12:19:14 +0900

    Nuget情報で未使用部を削除

--- Ann.GenOpenSourceList/Generator.cs
+++ Ann.GenOpenSourceList/Generator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -67,43 +66,18 @@ namespace Ann.GenOpenSourceList
         // ReSharper disable InconsistentNaming
         public class Rootobject
         {
-            public Context context { get; set; }
-            public int totalHits { get; set; }
-            public DateTime lastReopen { get; set; }
-            public string index { get; set; }
             public Datum[] data { get; set; }
-        }
-
-        public class Context
-        {
-            public string vocab { get; set; }
-            public string _base { get; set; }
         }
 
         public class Datum
         {
             public string id { get; set; }
-            public string type { get; set; }
-            public string registration { get; set; }
-            //public string id { get; set; }
-            public string version { get; set; }
             public string description { get; set; }
             public string summary { get; set; }
             public string title { get; set; }
-            public string iconUrl { get; set; }
-            public string licenseUrl { get; set; }
             public string projectUrl { get; set; }
             public string[] tags { get; set; }
             public string[] authors { get; set; }
-            public int totalDownloads { get; set; }
-            public Version[] versions { get; set; }
-        }
-
-        public class Version
-        {
-            public string version { get; set; }
-            public int downloads { get; set; }
-            public string id { get; set; }
         }
 
         // ReSharper restore InconsistentNaming
