f7fba157d8bfbcc919c01d1beade2dae4458c4ef
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-08 10:56:37 +0900

    Ann.Test追加

---
+++ Ann.Test/SettingWindow/SettingPage/About/AboutTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Ann.SettingWindow.SettingPage.About;
+using Xunit;
+
+namespace Ann.Test.SettingWindow.SettingPage.About
+{
+    public class AboutTest : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+
+        [Fact]
+        public void Basic()
+        {
+        }
+    }
+}
