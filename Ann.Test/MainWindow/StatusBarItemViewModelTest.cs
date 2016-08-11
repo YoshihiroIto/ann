9a6f8884c0c823eb843943b78a6c9691b723186b
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-11 15:00:57 +0900

    StatusBarItemViewModelTest追加

---
+++ Ann.Test/MainWindow/StatusBarItemViewModelTest.cs
@@ -0,0 +1,26 @@
+﻿using Ann.MainWindow;
+using Xunit;
+
+namespace Ann.Test.MainWindow
+{
+    public class StatusBarItemViewModelTest
+    {
+        [Fact]
+        public void Basic()
+        {
+            TestHelper.CleanTestEnv();
+
+            using (var vm = new StatusBarItemViewModel("ABC"))
+            {
+                Assert.Equal("ABC", vm.Message.Value);
+                Assert.Equal(StatusBarItemViewModel.SearchKey.NoKey, vm.Key);
+            }
+
+            using (var vm = new StatusBarItemViewModel(StatusBarItemViewModel.SearchKey.InOpening, "DEF"))
+            {
+                Assert.Equal("DEF", vm.Message.Value);
+                Assert.Equal(StatusBarItemViewModel.SearchKey.InOpening, vm.Key);
+            }
+        }
+    }
+}
