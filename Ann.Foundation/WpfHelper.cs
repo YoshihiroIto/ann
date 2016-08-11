69b33c90e68dccce12b9465bee581611a42e8495
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-11 23:10:20 +0900

    ショートカット追加時、追加アイテムにフォーカスを当てるようにした

--- Ann.Foundation/WpfHelper.cs
+++ Ann.Foundation/WpfHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Threading;
 
 namespace Ann.Foundation
@@ -31,6 +32,60 @@ namespace Ann.Foundation
             ((DispatcherFrame) f).Continue = false;
 
             return null;
+        }
+
+        //http://stackoverflow.com/questions/19523139/find-control-in-the-visual-tree 
+        public static DependencyObject FindChild(DependencyObject parent, string name)
+        {
+            if (parent == null || string.IsNullOrEmpty(name))
+                return null;
+
+            var element = parent as FrameworkElement;
+            if (element != null && element.Name == name)
+                return parent;
+
+            DependencyObject result = null;
+
+            (parent as FrameworkElement)?.ApplyTemplate();
+
+            var childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (var i = 0; i != childrenCount; ++i)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                result = FindChild(child, name);
+
+                if (result != null)
+                    break;
+            }
+
+            return result;
+        }
+
+        public static T FindChild<T>(DependencyObject parent) where T : DependencyObject
+        {
+            if (parent == null)
+                return null;
+
+            var findChild = parent as T;
+            if (findChild != null)
+                return findChild;
+
+            DependencyObject foundChild = null;
+
+            (parent as FrameworkElement)?.ApplyTemplate();
+
+            var childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+
+            for (var i = 0; i != childrenCount; ++i)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                foundChild = FindChild<T>(child);
+
+                if (foundChild != null)
+                    break;
+            }
+
+            return (T) foundChild;
         }
     }
 }
