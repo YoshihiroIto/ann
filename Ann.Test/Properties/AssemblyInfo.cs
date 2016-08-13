2b5d83a1937f8bb721f7aa144fda656b1f4f4561
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-13 22:44:19 +0900

    テストを並列実行しないようにした

--- Ann.Test/Properties/AssemblyInfo.cs
+++ Ann.Test/Properties/AssemblyInfo.cs
@@ -35,3 +35,5 @@ using Xunit;
 //[アセンブリ: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
