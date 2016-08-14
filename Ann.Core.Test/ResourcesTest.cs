7fdd76c864615aee511d3ae76582c72787218f22
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-14 14:38:50 +0900

    ResourcesTest 追加

---
+++ Ann.Core.Test/ResourcesTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using Xunit;
+using Ann.Core.Properties;
+
+namespace Ann.Core.Test
+{
+    public class ResourcesTest
+    {
+        [Fact]
+        public void Basic()
+        {
+            Assert.NotNull(Resources.ResourceManager.BaseName);
+            Assert.Null(Resources.Culture);
+
+            Resources.Culture = new CultureInfo("ja");
+            Assert.NotNull(Resources.Culture);
+
+            Assert.NotNull(Resources.OpenSourceList);
+            Assert.NotNull(Resources.OpenSourceListNonNuget);
+        }
+    }
+}
