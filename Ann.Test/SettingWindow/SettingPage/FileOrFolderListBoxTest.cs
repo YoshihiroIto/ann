23fe9e7e2335d7a1425def062763f0071eb06354
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-14 15:27:39 +0900

    FileOrFolderListBoxTest 追加

---
+++ Ann.Test/SettingWindow/SettingPage/FileOrFolderListBoxTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Windows;
+using Ann.Core;
+using Ann.SettingWindow.SettingPage;
+using Xunit;
+
+namespace Ann.Test.SettingWindow.SettingPage
+{
+    public class FileOrFolderListBoxTest
+    {
+        [WpfFact]
+        public void Basic()
+        {
+            Application.ResourceAssembly = Assembly.GetAssembly(typeof(App));
+
+            var e = new Entry();
+            e.InitializeComponent();
+
+            var c = new FileOrFolderListBox();
+        }
+    }
+}
