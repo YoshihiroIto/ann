fb0b10189aa1394af0b1b2a6c8e446d5b8fc4212
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-14 12:37:31 +0900

    WindowDisableMinMaxBoxBehavior 追加

--- Ann/SettingWindow/SettingWindow.xaml.cs
+++ Ann/SettingWindow/SettingWindow.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using System.Windows;
-using System.Windows.Interop;
-
-namespace Ann.SettingWindow
+﻿namespace Ann.SettingWindow
 {
     /// <summary>
     /// SettingWindow.xaml の相互作用ロジック
@@ -13,29 +8,6 @@ namespace Ann.SettingWindow
         public SettingWindow()
         {
             InitializeComponent();
-
-            Loaded += OnLoaded;
         }
-
-        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
-        {
-            var handle = new WindowInteropHelper(this).Handle;
-            var style = GetWindowLong(handle, GWL_STYLE);
-            style &= ~WS_MAXIMIZEBOX;
-            style &= ~WS_MINIMIZEBOX;
-            SetWindowLong(handle, GWL_STYLE, style);
-        }
-
-        // ReSharper disable InconsistentNaming
-        private const int GWL_STYLE = -16;
-        private const int WS_MAXIMIZEBOX = 0x10000;
-        private const int WS_MINIMIZEBOX = 0x20000;
-        // ReSharper restore InconsistentNaming
-
-        [DllImport("user32.dll")]
-        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-
-        [DllImport("user32.dll")]
-        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
     }
 }
