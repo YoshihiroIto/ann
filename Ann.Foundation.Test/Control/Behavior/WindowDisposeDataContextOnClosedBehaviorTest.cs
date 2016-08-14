6c3e7f2e2bcfdb5dcdd9161af0deb5946db2a22f
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-14 12:00:28 +0900

    WindowDisposeDataContextOnClosedBehaviorTest追加

---
+++ Ann.Foundation.Test/Control/Behavior/WindowDisposeDataContextOnClosedBehaviorTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Ann.Foundation.Control.Behavior;
+using Xunit;
+
+namespace Ann.Foundation.Test.Control.Behavior
+{
+    public class WindowDisposeDataContextOnClosedBehaviorTest
+    {
+        [WpfFact]
+        public void Basic()
+        {
+            var w = new Window();
+
+            var b = new WindowDisposeDataContextOnClosedBehavior();
+
+            b.Attach(w);
+
+            w.Close();
+
+            b.Detach();
+        }
+    }
+}
