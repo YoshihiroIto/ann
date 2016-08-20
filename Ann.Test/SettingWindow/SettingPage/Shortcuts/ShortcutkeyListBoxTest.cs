7fe0d9bac90e8a7d322cb6b5750f55a770b8d4af
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-20 13:18:02 +0900

    テスト更新

---
+++ Ann.Test/SettingWindow/SettingPage/Shortcuts/ShortcutkeyListBoxTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using Ann.Foundation;
+using Ann.SettingWindow.SettingPage.Shortcuts;
+using Reactive.Bindings;
+using Xunit;
+
+namespace Ann.Test.SettingWindow.SettingPage.Shortcuts
+{
+    public class ShortcutkeyListBoxTest : MarshalByRefObject, IDisposable
+    {
+        public void Dispose()
+        {
+            // for appveyor 
+            Dispatcher.CurrentDispatcher.InvokeShutdown();
+        }
+
+        [WpfFact]
+        public void Basic()
+        {
+            RunOnTestDomain.Do(() =>
+            {
+                Application.ResourceAssembly = Assembly.GetAssembly(typeof(Entry));
+
+                var entry = new Entry();
+                entry.InitializeComponent();
+
+                try
+                {
+                    var c = new ShortcutkeyListBox();
+
+                    using (var command = new ReactiveCommand())
+                    {
+                        c.AddCommand = command;
+                        c.RemoveCommand = command;
+
+                        Assert.Same(command, c.AddCommand);
+                        Assert.Same(command, c.RemoveCommand);
+                    }
+
+                    var l = new ObservableCollection<int>();
+                    c.Items = l;
+
+                    Assert.Same(l, c.Items);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception(e.Message + e.StackTrace);
+                }
+            });
+        }
+    }
+}
