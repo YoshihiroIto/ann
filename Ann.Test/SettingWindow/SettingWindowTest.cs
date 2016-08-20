c4ad8fccb0d3ffb223275ff2c55fbc60fe8d5296
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-08-20 13:26:37 +0900

    テスト更新

---
+++ Ann.Test/SettingWindow/SettingWindowTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Threading;
+using Ann.Foundation;
+using Xunit;
+
+namespace Ann.Test.SettingWindow
+{
+    public class SettingWindowTest : MarshalByRefObject, IDisposable
+    {
+        private readonly DisposableFileSystem _config = new DisposableFileSystem();
+
+        public void Dispose()
+        {
+            _config.Dispose();
+
+            // for appveyor 
+            Dispatcher.CurrentDispatcher.InvokeShutdown();
+        }
+
+        [WpfFact]
+        public void Basic()
+        {
+            RunOnTestDomain.Do(() =>
+            {
+                Application.ResourceAssembly = Assembly.GetAssembly(typeof(Entry));
+
+                var entry = new Entry();
+                entry.InitializeComponent();
+
+                try
+                {
+                    // ReSharper disable once ObjectCreationAsStatement
+                    new Ann.SettingWindow.SettingWindow();
+                }
+                catch (Exception e)
+                {
+                    throw new Exception(e.Message + e.StackTrace);
+                }
+            });
+        }
+    }
+}
