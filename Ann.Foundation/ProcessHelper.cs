db5bb15fe39b224e176381a5528c9ea2f8ec4402
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-09-10 21:10:35 +0900

    ショートカットが開けないことがあったのを修正

--- Ann.Foundation/ProcessHelper.cs
+++ Ann.Foundation/ProcessHelper.cs
@@ -1,5 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.IO;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using IWshRuntimeLibrary;
+using File = System.IO.File;
 
 namespace Ann.Foundation
 {
@@ -9,17 +13,20 @@ namespace Ann.Foundation
         {
             return await Task.Run(() =>
             {
-                var info = new ProcessStartInfo(cmd)
-                {
-                    Arguments = args
-                };
+                var isShortcutFile = string.Equals(Path.GetExtension(cmd), ".lnk", StringComparison.OrdinalIgnoreCase);
+                var startInfo = isShortcutFile
+                    ? MakeProcessStartInfoForShortcut(cmd)
+                    : new ProcessStartInfo(cmd) { Arguments = args };
+
+                if (startInfo == null)
+                    return false;
 
                 if (isRunAsAdmin)
-                    info.Verb = "runas";
+                    startInfo.Verb = "runas";
 
                 try
                 {
-                    Process.Start(info);
+                    Process.Start(startInfo);
                     return true;
                 }
                 catch
@@ -29,6 +36,33 @@ namespace Ann.Foundation
 
                 return false;
             });
+        }
+
+        private static ProcessStartInfo MakeProcessStartInfoForShortcut(string shortcutFilePath)
+        {
+            // http://stackoverflow.com/questions/19523419/unable-to-launch-shortcut-lnk-files-from-32-bit-c-sharp-application-when-the-f
+
+            var shell = new IWshShell_Class();
+            var shortcut = (IWshShortcut_Class)shell.CreateShortcut(shortcutFilePath);
+
+            var cmdPath = shortcut.TargetPath;
+
+            if (File.Exists(cmdPath) == false)
+                cmdPath = cmdPath.Replace("SysWOW64", "System32");
+
+            if (File.Exists(cmdPath) == false)
+                cmdPath = cmdPath.Replace("Program Files (x86)", "Program Files");
+
+            if (File.Exists(cmdPath) == false)
+                return null;
+
+            var startInfo = new ProcessStartInfo(cmdPath)
+            {
+                WorkingDirectory = shortcut.WorkingDirectory,
+                Arguments = shortcut.Arguments
+            };
+
+            return startInfo;
         }
     }
 }
