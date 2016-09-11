09e39f12e5dc1596bb7614747a0f8c0f9d439ecf
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-09-11 21:01:41 +0900

    描画テキストオブジェクトをキャッシュするようにした

--- Ann/MainWindow/CandidatePanelCanvas.cs
+++ Ann/MainWindow/CandidatePanelCanvas.cs
@@ -3,13 +3,14 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using Ann.Core;
+using Jewelry.Collections;
+using Application = System.Windows.Application;
+using FlowDirection = System.Windows.FlowDirection;
 
 namespace Ann.MainWindow
 {
     public class CandidatePanelCanvas : Canvas
     {
-        private static readonly Typeface Dummy = new Typeface(string.Empty);
-
         private static readonly Pen PanelBorderLinePen;
         private static readonly Brush CaptionBrush;
         private static readonly Brush CommentBrush;
@@ -39,42 +40,38 @@ namespace Ann.MainWindow
             if (d == null)
                 return;
 
-            var backgroundRect = new Rect
             {
-                X = 0,
-                Y = 0,
-                Width = ViewConstants.CandidatePanelWidth,
-                Height = ViewConstants.CandidatePanelHeight
-            };
+                var backgroundRect = new Rect
+                {
+                    X = 0,
+                    Y = 0,
+                    Width = ViewConstants.CandidatePanelWidth,
+                    Height = ViewConstants.CandidatePanelHeight
+                };
 
-            if (d.IsSelected)
-                dc.DrawRectangle(IsSelectedBackgroundBrush, null, backgroundRect);
-            else if (IsMouseOver)
-                dc.DrawRectangle(IsMouseOverBackgroundBrush, null, backgroundRect);
-            else
-                dc.DrawRectangle(TransparentBrush, null, backgroundRect);
+                if (d.IsSelected)
+                    dc.DrawRectangle(IsSelectedBackgroundBrush, null, backgroundRect);
+                else if (IsMouseOver)
+                    dc.DrawRectangle(IsMouseOverBackgroundBrush, null, backgroundRect);
+                else
+                    dc.DrawRectangle(TransparentBrush, null, backgroundRect);
+            }
 
-            var p0 = new Point(0, ViewConstants.CandidatePanelHeight);
-            var p1 = new Point(ViewConstants.CandidatePanelWidth, ViewConstants.CandidatePanelHeight);
-            dc.DrawLine(PanelBorderLinePen, p0, p1);
-
+            {
+                var p0 = new Point(0, ViewConstants.CandidatePanelHeight);
+                var p1 = new Point(ViewConstants.CandidatePanelWidth, ViewConstants.CandidatePanelHeight);
+                dc.DrawLine(PanelBorderLinePen, p0, p1);
+            }
 
             if (d.Name != null)
             {
-                var text = new FormattedText(
+                var text = MakeText(
                     d.Name,
-                    CultureInfo.CurrentUICulture,
-                    FlowDirection.LeftToRight,
-                    Dummy,
+                    ViewConstants.CandidatePanel_Name_Width,
+                    30,
+                    CaptionBrush,
                     22,
-                    CaptionBrush)
-                {
-                    MaxTextWidth = ViewConstants.CandidatePanel_Name_Width,
-                    MaxTextHeight = 30,
-                    Trimming = TextTrimming.CharacterEllipsis
-                };
-
-                text.SetFontFamily(Application.Current.MainWindow.FontFamily);
+                    NameTextCache);
 
                 var p = new Point(ViewConstants.CandidatePanel_Name_Left, ViewConstants.CandidatePanel_Name_Top);
                 dc.DrawText(text, p);
@@ -82,20 +79,13 @@ namespace Ann.MainWindow
 
             if (d.Comment != null)
             {
-                var text = new FormattedText(
+                var text = MakeText(
                     d.Comment,
-                    CultureInfo.CurrentUICulture,
-                    FlowDirection.LeftToRight,
-                    Dummy,
+                    ViewConstants.CandidatePanel_Comment_Width,
+                    16,
+                    CommentBrush,
                     11,
-                    CommentBrush)
-                {
-                    MaxTextWidth = ViewConstants.CandidatePanel_Comment_Width,
-                    MaxTextHeight = 16,
-                    Trimming = TextTrimming.CharacterEllipsis
-                };
-
-                text.SetFontFamily(Application.Current.MainWindow.FontFamily);
+                    CommentTextCache);
 
                 var p = new Point(ViewConstants.CandidatePanel_Comment_Left, ViewConstants.CandidatePanel_Comment_Top);
                 dc.DrawText(text, p);
@@ -113,5 +103,32 @@ namespace Ann.MainWindow
                 dc.DrawRectangle(d.Icon, null, r);
             }
         }
+
+        private static FormattedText MakeText(string src, double width, double height, Brush brush, double emSize, LruCache<string, FormattedText> cache)
+        {
+            return cache.GetOrAdd(src, s =>
+            {
+                var text = new FormattedText(
+                    s,
+                    CultureInfo.CurrentUICulture,
+                    FlowDirection.LeftToRight,
+                    Dummy,
+                    emSize,
+                    brush)
+                {
+                    MaxTextWidth = width,
+                    MaxTextHeight = height,
+                    Trimming = TextTrimming.CharacterEllipsis
+                };
+
+                text.SetFontFamily(Application.Current.MainWindow.FontFamily);
+
+                return text;
+            });
+        }
+
+        private static readonly Typeface Dummy = new Typeface(string.Empty);
+        private static readonly LruCache<string, FormattedText> NameTextCache = new LruCache<string, FormattedText>(50, false);
+        private static readonly LruCache<string, FormattedText> CommentTextCache = new LruCache<string, FormattedText>(50, false);
     }
 }
